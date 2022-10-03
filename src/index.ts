06b3e7a290eb8f45f860b521db4deb63231a7813
Bruna Nunes <bruna.f.n@hotmail.com>
2022-10-03 14:50:58 -0300

    adicionando cors

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,13 @@ import routes from './routes';
 AppDataSource.initialize()
   .then(() => {
     const app = express();
+    const cors = require("cors");
+    
+    app.use(cors());
 
     app.use(express.json());
 
-    app.use(routes);
+    app.use('/api', routes);
 
     return app.listen(process.env.PORT);
   })
